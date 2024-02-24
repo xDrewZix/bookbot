567f808188604cd1e0112cbce40b5981b2320560
xDrewZix <andrewjfcrawshaw@hotmail.co.uk>
2024-02-24 19:47:21 +0000

    added function to count the amount of times each letter appears, includes import for string module, so that I have a list to compare against

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
 
     book_path = "books/Frankenstein.txt"
     text = get_book_text(book_path)
-    text = count_words(text)
+    count_letter = count_each_letter(text)
     print(text)
 
 def get_book_text(path):
@@ -15,5 +15,17 @@ def count_words(text):
     text = text.split()
     count = len(text)
     return count
+
+def count_each_letter(text):
+    alphabet = list(string.ascii_lowercase)
+    letter_dict = {}
+    text = text.lower()
+    for letter in text:
+        if letter in alphabet:
+            if letter not in letter_dict:
+                letter_dict[letter] = 1
+            else:
+                letter_dict[letter] +=1
+    return letter_dict
 
 main()
