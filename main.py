42c7d1e95ae632c91ab8877a9569d916e1fa7d04
xDrewZix <andrewjfcrawshaw@hotmail.co.uk>
2024-02-24 20:13:06 +0000

    Added comments to code

--- main.py
+++ main.py
@@ -18,15 +18,18 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
 def sort_on(dict):
-    return dict["count"]
+    return dict["count"] 
 
 def count_words(text):
     text = text.split()
     count = len(text)
     return count
 
+
 def dict_to_list(dict):
+    '''changes the dict into a list of dictionaries for easier sorting'''
     list_of_dicts = []
     for letter, count in dict.items():
         add_dict = {'letter': letter, 'count': count}
@@ -34,12 +37,16 @@ def dict_to_list(dict):
     return list_of_dicts
 
 def list_to_dict(listdict):
+    '''changes the list back to a dictionary for easier iteration in main() for-loop.'''
     sorted_dict = {}
     for i in listdict:
         sorted_dict[i['letter']] = i['count']
     return sorted_dict
 
+
 def count_each_letter(text):
+    '''adds each letter as a key and its count as value to a dictionary
+    alphabet is compared against the letter to make sure it is a letter.'''
     alphabet = list(string.ascii_lowercase)
     letter_dict = {}
     text = text.lower()
