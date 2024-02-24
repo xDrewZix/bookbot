19e11aeb797dcb20b2b463e8f6c640d5b3017522
xDrewZix <andrewjfcrawshaw@hotmail.co.uk>
2024-02-24 19:39:05 +0000

    added a function to count the words in opened text

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ def main():
 
     book_path = "books/Frankenstein.txt"
     text = get_book_text(book_path)
+    text = count_words(text)
     print(text)
 
 def get_book_text(path):
 
     with open(path) as f:
         return f.read()
+
+def count_words(text):
+    text = text.split()
+    count = len(text)
+    return count
 
 main()
